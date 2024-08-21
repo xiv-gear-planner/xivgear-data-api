5b92268e70d9a84927325e2468ed5a1218223b7e
XP <smurfs@smurfs.verymad.net>
2024-08-21 15:50:43 -0700

    Periodic reload

--- src/main/groovy/gg/xp/xivgear/dataapi/datamanager/DataManager.groovy
+++ src/main/groovy/gg/xp/xivgear/dataapi/datamanager/DataManager.groovy
@@ -2,6 +2,7 @@ package gg.xp.xivgear.dataapi.datamanager
 
 import gg.xp.xivapi.XivApiClient
 import gg.xp.xivapi.clienttypes.XivApiObject
+import gg.xp.xivapi.clienttypes.XivApiSchemaVersion
 import gg.xp.xivapi.filters.SearchFilter
 import gg.xp.xivapi.pagination.ListOptions
 import gg.xp.xivgear.dataapi.models.*
@@ -9,22 +10,26 @@ import groovy.util.logging.Slf4j
 import io.micronaut.context.annotation.Context
 import jakarta.inject.Singleton
 
-import java.util.concurrent.Callable
-import java.util.concurrent.ExecutorService
-import java.util.concurrent.Executors
-import java.util.concurrent.Future
+import java.time.Duration
+import java.util.concurrent.*
 
 import static gg.xp.xivapi.filters.SearchFilters.*
 
+/**
+ * The main data holder class. Responsible for gathering data from xivapi, holding onto it, and occasionally refreshing
+ * it.
+ */
 @Context
 @Singleton
 @Slf4j
-class DataManager {
+class DataManager implements AutoCloseable {
 
 	private static final ExecutorService exs = Executors.newVirtualThreadPerTaskExecutor()
 
-	private final Future<FullData> dataFuture
+	private Future<FullData> dataFuture
+	private final Thread updater
 	private final XivApiClient client
+	private volatile boolean stop
 
 	private static final int minIlvl = 290
 //	private static final int minIlvl = 570
@@ -38,6 +43,65 @@ class DataManager {
 	DataManager() {
 		client = new XivApiClient()
 		dataFuture = exs.submit(this.&makeData as Callable<FullData>)
+		updater = Thread.startVirtualThread this.&updateLoop
+	}
+
+	Runnable updateLoop() {
+		while (!stop) {
+			try {
+				// TODO: do self-testing of the newly-acquired data to make sure that a schema mismatch didn't
+				// horribly break things.
+				Thread.sleep(Duration.ofSeconds(60).toMillis())
+				// If the initial load failed, save new data unconditionally
+
+				Future.State state = dataFuture.state()
+				if (state == Future.State.FAILED || state == Future.State.CANCELLED) {
+					log.info("Initial load failed, updater trying again")
+					FullData data = makeData()
+					dataFuture = CompletableFuture.completedFuture(data)
+				}
+				else if (state == Future.State.RUNNING) {
+					log.info("Skipping update because initial load is still in progress")
+				}
+				else {
+					FullData oldData = dataFuture.get()
+					// First, check if there's actually an update
+					Set<String> oldVersions = oldData.versions.toSet()
+					Set<String> newVersions = client.gameVersions.toSet()
+					XivApiSchemaVersion oldSchema = oldData.schemaVersion
+					XivApiSchemaVersion newSchema = getLiveSchemaVersion()
+					boolean versionSame = oldVersions == newVersions
+					boolean schemaSame = oldSchema.fullVersionString() == newSchema.fullVersionString()
+					if (versionSame && schemaSame) {
+						log.info("No update")
+					}
+					else {
+						if (versionSame) {
+							log.info("Update triggered (schema changed), going to reload data")
+						}
+						else if (schemaSame) {
+							log.info("Update triggered (version changed), going to reload data")
+						}
+						else {
+							log.info("Update triggered (both changed), going to reload data")
+						}
+						FullData data = makeData()
+						dataFuture = CompletableFuture.completedFuture(data)
+						log.info("Reloaded data")
+					}
+				}
+			}
+			catch (Throwable t) {
+				if (!stop) {
+					log.error("Error in update loop", t)
+					Thread.sleep(10_000)
+				}
+			}
+		}
+	}
+
+	private XivApiSchemaVersion getLiveSchemaVersion() {
+		return client.getById(BaseParam, 1).schemaVersion
 	}
 
 	private FullData makeData() {
@@ -48,6 +112,10 @@ class DataManager {
 				build()
 			}
 			client.defaultListOpts = opts
+
+			log.info "Loading versions"
+			List<String> versions = client.gameVersions
+			log.info "Loaded ${versions.size()} versions"
 
 			log.info "Loading BaseParams"
 			List<BaseParam> baseParams = client.getListIterator(BaseParam).toList().toSorted { it.rowId }
@@ -111,7 +179,7 @@ class DataManager {
 			}.<Food> toSorted { it.rowId }
 			log.info "Loaded ${food.size()} Foods"
 
-			return new FullData(baseParams, itemBases, itemLevels, jobs, materia, food)
+			return new FullData(versions, baseParams, itemBases, itemLevels, jobs, materia, food)
 		}
 		catch (Throwable t) {
 			log.error("Error getting data", t)
@@ -133,5 +201,12 @@ class DataManager {
 
 	Future<FullData> getDataFuture() {
 		return dataFuture
+	}
+
+	@Override
+	void close() throws Exception {
+		stop = true
+		updater.interrupt()
+		client.close()
 	}
 }
--- src/main/groovy/gg/xp/xivgear/dataapi/datamanager/FullData.groovy
+++ src/main/groovy/gg/xp/xivgear/dataapi/datamanager/FullData.groovy
@@ -1,5 +1,6 @@
 package gg.xp.xivgear.dataapi.datamanager
 
+import gg.xp.xivapi.clienttypes.XivApiSchemaVersion
 import gg.xp.xivgear.dataapi.models.*
 import groovy.transform.TupleConstructor
 
@@ -9,6 +10,7 @@ class FullData implements Serializable {
 	@Serial
 	static final long serialVersionUID = 1
 
+	final List<String> versions
 	final List<BaseParam> baseParams
 	final List<ItemBase> itemBases
 	final List<ItemLevel> itemLevels
@@ -16,5 +18,16 @@ class FullData implements Serializable {
 	final List<Materia> materia
 	final List<Food> food
 
+	XivApiSchemaVersion getSchemaVersion() {
+		return baseParams[0].schemaVersion
+	}
 
+	@Override
+	Object getProperty(String propertyName) {
+		Object out = super.getProperty(propertyName)
+		if (out instanceof List) {
+			return Collections.unmodifiableList(out)
+		}
+		return out
+	}
 }
