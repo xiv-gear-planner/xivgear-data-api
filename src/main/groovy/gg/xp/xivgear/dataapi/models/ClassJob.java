f84204df288495d9ecaf67d8c9b55a7c1193167d
XP <smurfs@smurfs.verymad.net>
2025-05-18 14:12:33 -0700

    Add ClassJob translations

--- src/main/groovy/gg/xp/xivgear/dataapi/models/ClassJob.java
+++ src/main/groovy/gg/xp/xivgear/dataapi/models/ClassJob.java
@@ -1,12 +1,21 @@
 package gg.xp.xivgear.dataapi.models;
 
+import gg.xp.xivapi.annotations.XivApiField;
 import gg.xp.xivapi.annotations.XivApiSheet;
+import gg.xp.xivapi.clienttypes.XivApiLangValue;
 import gg.xp.xivapi.clienttypes.XivApiObject;
 
 @XivApiSheet
 public interface ClassJob extends XivApiObject {
 
 	String getAbbreviation();
+
+	@XivApiField("Abbreviation")
+	XivApiLangValue<String> getAbbreviationTranslations();
+
+	@XivApiField("Name")
+	XivApiLangValue<String> getNameTranslations();
+
 	int getModifierDexterity();
 	int getModifierHitPoints();
 	int getModifierIntelligence();
